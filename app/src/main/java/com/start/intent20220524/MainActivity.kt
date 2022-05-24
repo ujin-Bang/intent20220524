cbc09963223751ea7eb6ef426855adc727e98523
ujin-Bang <qkddnwls14@gmail.com>
2022-05-24 20:35:19 +0900

    네이버 웹 링크 연결

--- app/src/main/java/com/start/intent20220524/MainActivity.kt
+++ app/src/main/java/com/start/intent20220524/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.btnMoveToOther.setOnClickListener {
             val myIntent = Intent(this, OtherActivity::class.java)
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
 
             val myIntent = Intent(this, ViewMessageActivity::class.java)
 
-            myIntent.putExtra("message",inputMessage)
+            myIntent.putExtra("message", inputMessage)
             startActivity(myIntent)
 
         }
@@ -46,7 +46,7 @@ class MainActivity : AppCompatActivity() {
 
 //            제공할 정보 2가지. 1)어떤 화면을로 갈건가?(ACTION) / 2)세부정보(전화걸기 - 어디로 전화거는가?) - Uri
             val myUri = Uri.parse("tel:${inputPhoneNum}")
-            val myIntent = Intent( Intent.ACTION_DIAL, myUri )
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
             startActivity(myIntent)
         }
 
@@ -64,7 +64,13 @@ class MainActivity : AppCompatActivity() {
             val myUri = Uri.parse("smsto:${phoneNum}")
             val myIntent = Intent(Intent.ACTION_SENDTO, myUri)
 
-            myIntent.putExtra("sms_body","자동으로입력할 문구에요")
+            myIntent.putExtra("sms_body", "자동으로입력할 문구에요")
+            startActivity(myIntent)
+        }
+
+        binding.btnNaver.setOnClickListener {
+            val myUri = Uri.parse("http://www.naver.com/")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
             startActivity(myIntent)
         }
     }
@@ -73,16 +79,16 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
 //        결과를 받아서 돌아오면 실행되는 함수.
-        Log.d("메인화면","결과를 받아오면 무조건 실행")
+        Log.d("메인화면", "결과를 받아오면 무조건 실행")
 
 //        구별해야 할 요소 1)어떤걸 가지러 다녀온건지? - requestCode 2) 확인을 누른건지 / 취소(빽버튼)를 누른건지 구별 3)첨부한 데이터
-        Log.d("리퀘스트코드",requestCode.toString())
+        Log.d("리퀘스트코드", requestCode.toString())
 
 //        닉네임을 가지러 다녀온게 맞는가?
-        if( requestCode == REQ_CODE_FOR_NICKNAME) {
+        if (requestCode == REQ_CODE_FOR_NICKNAME) {
 
 //            확인이 누린게 맞는가?
-            if(resultCode == RESULT_OK) {
+            if (resultCode == RESULT_OK) {
 
 //                담아준 결과용 Intent => data에 담겨 있다. => "nick"으로 이름붙인 String을 꺼내주자.
                 val newNickname = data?.getStringExtra("nick")
