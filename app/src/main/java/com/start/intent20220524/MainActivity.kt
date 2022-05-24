12f89010159aca87f96b6797dd12f2e9e540587f
ujin-Bang <qkddnwls14@gmail.com>
2022-05-24 18:21:46 +0900

    메인 화면 복귀 - 새 닉네임 반영  - onActivityReault 함수사용  - request종류 구별해서 if  - result종류 구별해서 if  - data에서 StringExtra꺼내서 사용

--- app/src/main/java/com/start/intent20220524/MainActivity.kt
+++ app/src/main/java/com/start/intent20220524/MainActivity.kt
@@ -3,6 +3,7 @@ package com.start.intent20220524
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.start.intent20220524.databinding.ActivityMainBinding
 
@@ -36,4 +37,29 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(myIntent, REQ_CODE_FOR_NICKNAME)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+//        결과를 받아서 돌아오면 실행되는 함수.
+        Log.d("메인화면","결과를 받아오면 무조건 실행")
+
+//        구별해야 할 요소 1)어떤걸 가지러 다녀온건지? - requestCode 2) 확인을 누른건지 / 취소(빽버튼)를 누른건지 구별 3)첨부한 데이터
+        Log.d("리퀘스트코드",requestCode.toString())
+
+//        닉네임을 가지러 다녀온게 맞는가?
+        if( requestCode == REQ_CODE_FOR_NICKNAME) {
+
+//            확인이 누린게 맞는가?
+            if(resultCode == RESULT_OK) {
+
+//                담아준 결과용 Intent => data에 담겨 있다. => "nick"으로 이름붙인 String을 꺼내주자.
+                val newNickname = data?.getStringExtra("nick")
+                binding.txtNickname.text = newNickname
+
+            }
+
+        }
+    }
+
 }
